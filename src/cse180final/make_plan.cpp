740ae2decf1b38f89e640bdb7433c12880715f23
Michael T. Andemeskel <mtandemeskel@gmail.com>
2017-04-21 16:47:28 -0700

    we have a local copy of global costmap, just the map part too

--- src/cse180final/make_plan.cpp
+++ src/cse180final/make_plan.cpp
@@ -1,20 +1,28 @@
 // http://wiki.ros.org/map_server?distro=kinetic
 #include <ros/ros.h>
+#include <ros/console.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <nav_msgs/MapMetaData.h>
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <typeinfo>
+#include <fstream>
 
 using namespace std;
 
 
 #define NODE_NAME "make_plan"
 // the occupancy grid of the map
-#define TOPIC_MAP "/map"
+// #define TOPIC_MAP "/move_base/global_costmap/costmap"
+#define TOPIC_MAP "map"
 // map metadata
 #define TOPIC_MAP_META "/map_metadata"
 
 
 bool DEBUGGING = true;
+int * cost_map;
+
 
 // mapCallback function
 void mapCallback( const nav_msgs::OccupancyGrid & msg );
@@ -29,8 +37,8 @@ int main( int argc, char ** argv ) {
   	ros::NodeHandle nh;
 
 	// create the Subscriber and set the topic to listen to
-	// ros::Subscriber sub_map = nh.subscribe( TOPIC_MAP, 1000, &mapCallback );
-	ros::Subscriber sub_map_meta = nh.subscribe( TOPIC_MAP_META, 1000, &mapMetaCallback );
+	ros::Subscriber sub_map = nh.subscribe( TOPIC_MAP, 1000, &mapCallback );
+	// ros::Subscriber sub_map_meta = nh.subscribe( TOPIC_MAP_META, 1000, &mapMetaCallback );
 
 	// listen to topic
 	ros::spin();
@@ -43,11 +51,85 @@ int main( int argc, char ** argv ) {
 // http://docs.ros.org/api/nav_msgs/html/msg/OccupancyGrid.html
 void mapCallback( const nav_msgs::OccupancyGrid & msg ) {
 
+	// if( DEBUGGING ) {
+		
+	// 	ROS_ERROR_STREAM( setprecision(2) << msg );
+		
+	// }
+
+	int width = msg.info.width;
+	int height = msg.info.height;
+	int size = width * height;
+	bool is_first_open = false;
+	// First map cell: 1582129
+	// End map cell: 2387754
+	int first_open = 0;
+	int last_open = 0;
+	int unknowns = 0;
+
+
 	if( DEBUGGING ) {
-		
-		ROS_ERROR_STREAM( setprecision(2) << msg );
-		
+
+		ROS_ERROR_STREAM( setprecision(2) << width );
+		ROS_ERROR_STREAM( setprecision(2) << height );
+		// 3936256 size of msg.data
+		ROS_ERROR_STREAM( setprecision(2) << msg.data.size() );
+
 	}
+
+	// get first and last cells of map
+	for( int y = 0; y < size; y++ ) {
+
+		if( (int)msg.data[y] != -1 && !is_first_open && first_open == 0 ) {
+
+			is_first_open = true;
+			first_open = y;
+
+		} else if ( (int)msg.data[y] != -1 && !is_first_open ) {
+
+			is_first_open = true;
+		
+		} else if(  (int)msg.data[y] != -1 && is_first_open ) {
+
+			last_open = y;
+			is_first_open = false;
+
+		}
+
+	}
+
+	if( DEBUGGING )
+		ROS_ERROR_STREAM( "first " << first_open << " last " << last_open );
+
+	// create our copy of the costmap
+	if( cost_map == NULL )
+		cost_map = new int[last_open - first_open];
+
+	// ofstream file;
+	// file.open( "map.txt" );
+
+	int x = 0;
+	for( int y = first_open; y <= last_open; y++ ) {
+	
+		if( (int)msg.data[y] == -1 ) 
+			unknowns++;
+		else if( unknowns > 20 )
+			unknowns = 0;
+
+		if( unknowns < 20 ) {
+
+			// ROS_ERROR_STREAM( "Cell: " << y << " " << (int)msg.data[y] );
+			// file << "Cell: " << y << " " << (int)msg.data[y] << endl;
+
+			// add cells to cost_map
+			cost_map[x] = (int)msg.data[y];
+			x++;
+
+		}
+
+	}
+
+	// file.close();
 
 }
 
