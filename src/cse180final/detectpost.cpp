fc496e39f9fbd838a0215abf9fb6f18a275a7b77
Michael Andemeskel <mandemeskel@gmail.com>
2017-04-26 19:55:42 -0700

    added code to detect posts

--- src/cse180final/detectpost.cpp
+++ src/cse180final/detectpost.cpp
@@ -2,6 +2,7 @@
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -10,6 +11,33 @@ using namespace std;
 #define TOPIC_NAME "/scan"
 
 bool DEBUGGING = true;
+const int UNDEFINED = -1;
+
+struct Post {
+	int start_index;
+	int stop_index;
+	int index_size;
+	float range;
+	float x;
+	float y;
+	float angle;
+	
+	Post() {
+		start_index = 0;
+		stop_index = 0;
+		x = 0;
+		y = 0;
+		angle = 0;
+	}
+
+	Post( int start, int stop, int count ) {
+		start_index = start;	
+		stop_index = stop;
+		index_size = count;
+	}
+
+};
+
 
 // callback function
 void callback( const sensor_msgs::LaserScan & msg );
@@ -21,24 +49,97 @@ int main( int argc, char ** argv ) {
   	ros::init( argc, argv, NODE_NAME );
   	ros::NodeHandle nh;
 
-	  // create the Subscriber and set the topic to listen to
-	  ros::Subscriber sub = nh.subscribe( TOPIC_NAME, 1000, &callback );
+	// create the Subscriber and set the topic to listen to
+	ros::Subscriber sub = nh.subscribe( TOPIC_NAME, 1000, &callback );
 
-	  // listen to topic
-	  ros::spin();
+	// listen to topic
+	ros::spin();
 
 	return 0;
 
 }
 
 
+const int MIN_POST_ZEROS = 150; // 167 is the real number
+vector<Post *> posts;
+float ANGLE_CONVERTER = 0.375;
 void callback( const sensor_msgs::LaserScan & msg ) {
 
 	if( DEBUGGING ) {
 		
-		ROS_ERROR_STREAM( setprecision(2) << msg );
+		// ROS_INFO( setprecision(2) << msg );
 		
 	}
+
+    	// loop through intensities to detect post signatures
+	int zero_count = 0;
+	int start_index = NULL;
+	int stop_index = NULL;
+	vector<Post *> recent_posts;
+	for( int n = 0; n < msg.intensities.size(); n++ ) {
+	    
+		if( (float)msg.intensities[n] == 0.0 ) {
+
+			zero_count++;			
+			if( start_index == NULL )
+				start_index = n;
+
+		} else if( zero_count >= MIN_POST_ZEROS ) {
+
+			recent_posts.push_back( new Post( start_index, stop_index, count ) );
+			count = 0;
+			start_index = NULL;			
+			stop_index = NULL;
+		
+		} else {
+
+			count = 0;
+			start_index = NULL;			
+			stop_index = NULL;
+			
+		}		
+
+	    
+	}
+
+
+	// calculate position of recently discovered posts
+	int index, range;
+	for( int n = 0; n < recent_posts.size(); n++ ) {
+
+		index = (recent_posts[n]->distance / 2) + recent_posts[n]->start_index;
+		range = msg.ranges[ index ];
+
+		// calculate angle
+		if( index < 360 ) {
+
+			recent_posts[n]->angle = 135 - (index * ANGLE_CONVERTER);
+
+		} else if( index > 360 ) {
+
+			recent_posts[n]->angle = ANGLE_CONVERTER * (index - 360);
+
+		} else {
+
+			recent_posts[n]->angle = 0;
+		
+		}
+
+		// calculate x, y position of post
+		// TODO: need to use frame transform matrices to get position of post
+		// TODO: right now it is relative to robot but we need it relative to map
+		// TODO: [POST][ROBOT][MA] = [POST_ACTUAL]	
+		recent_posts[n]->x = range * cosf(angle);
+		recent_posts[n]->y = range * sinf(angle);;
+		recent_posts[n]->range = range;
+		// ROS_INFO( "Post: ( %f, %f), Range: %f, Angle: " )
+		ROS_ERROR_STREAM( (*recent_posts[n]) );
+
+	}	
+	
+
+	if( recent_posts.size() == 0 )
+		ROS_ERROR_STREAM( "no posts detected" );
 
 }
 
